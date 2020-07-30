b28672d48be299b2e5a111a81aea7f90ba2352cb
Aska2790 <arslanannaev7@gmail.com>
2020-07-30 21:02:47 +0300

    Пустой проект

---
+++ src/main/java/com/aska/development/rxjava/Runner.java
@@ -0,0 +1,18 @@
+package com.aska.development.rxjava;
+
+public class Runner {
+    //region Fields
+    //endregion
+
+    //region Properties
+    //endregion
+
+    //region Constructors
+    //endregion
+
+    //region Methods
+    //endregion
+
+    //region Inner 
+    //endregion
+}
