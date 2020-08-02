6d8c5e9d18763fe88f5c6a7f87723b384c1d802c
Aska2790 <arslanannaev7@gmail.com>
2020-08-02 20:47:13 +0300

    lesson 2.2 loops

---
+++ Kotlin basics/src/lesson_2_2/Main.kt
@@ -0,0 +1,22 @@
+package lesson_2_2
+
+fun main() {
+
+    println("while loop")
+    var i = 1
+    while (i < 10) {
+        print(i)
+        i += 1
+    }
+
+    println("----------------")
+
+    println("do while loop")
+    var j = 0
+    do {
+        print(j)
+        j += 1
+    }while (j < 10)
+    
+    println("----------------")
+}
