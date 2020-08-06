0f1b741eaa121dfbb54faab8fdbf9c7ce202da4c
Aska2790 <arslanannaev7@gmail.com>
2020-08-06 21:47:52 +0300

    lesson 7.10 extensions

---
+++ Kotlin basics/src/lesson_7_10/Main.kt
@@ -0,0 +1,5 @@
+package lesson_7_10
+
+fun main() {
+    println("Last char of string ${"text".lastChar()}")
+}
---
+++ Kotlin basics/src/lesson_7_10/Utils.kt
@@ -0,0 +1,5 @@
+package lesson_7_10
+
+fun String.lastChar():Char{
+    return this.get(this.length -1);
+}
