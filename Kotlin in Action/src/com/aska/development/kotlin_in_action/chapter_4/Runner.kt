3be32c783e219c864e5d3b6558c67757273d5344
Aska2790 <arslanannaev7@gmail.com>
2020-08-15 22:08:23 +0300

    sealed class

--- Kotlin in Action/src/com/aska/development/kotlin_in_action/chapter_4/Runner.kt
+++ Kotlin in Action/src/com/aska/development/kotlin_in_action/chapter_4/Runner.kt
@@ -1,5 +1,11 @@
 package com.aska.development.kotlin_in_action.chapter_4
 
+sealed class Expression
+
+data class Num(val number: Double): Expression()
+data class Sum(val expression1: Expression, val expression2: Expression): Expression()
+object NotNumber:Expression()
+
 fun main() {
     val button: Clickable = Button()
     val imageButton:Clickable = ImageButton()
@@ -37,6 +43,22 @@ fun main() {
     println("======================================================")
 
     button.log("log message")
+
+    println("======================================================")
+
+    val number1: Num =  Num(5.0)
+    val number2: Num =  Num(5.5)
+
+    fun eval(e:Expression): Double {
+        val d = when (e) {
+            is Num -> e.number
+            is Sum -> eval(e.expression1) + eval(e.expression2)
+            is NotNumber -> Double.NaN
+        }
+        return d
+    }
+
+    println("Sum ${number1.number} + ${number2.number} = ${eval(Sum(number1, number2))}")
 
     println("======================================================")
 
